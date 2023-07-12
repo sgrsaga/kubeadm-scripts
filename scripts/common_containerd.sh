16b4719dc7cf0fba2451eb009fc4348fec8ca4d4
A.M Sagara Jayathilaka <sgrsagara@gmail.com>
2023-07-12 07:45:44 +0530

    Update common_containerd.sh

--- scripts/common_containerd.sh
+++ scripts/common_containerd.sh
@@ -43,7 +43,7 @@ sudo sysctl --system
 ############ Install containerd ###########
 #Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 #Add Docker’s official GPG key:
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -55,7 +55,10 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install containerd.io -y
+sudo apt-get install containerd -y
+
+sudo mkdir -p /etc/containerd/
+containerd config default | sudo tee /etc/containerd/config.toml
 
 sudo systemctl daemon-reload
 sudo systemctl enable containerd --now
